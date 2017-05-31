ff7469f78bf2fc736713822788e47041780c7e7b
gkaigk1987 <gkaigk1987@gmail.com>
2017-05-31 16:01:11 +0800

    service

---
+++ src/main/java/gk/springboot/com/service/IUserService.java
@@ -0,0 +1,14 @@
+package gk.springboot.com.service;
+
+import java.util.List;
+
+import gk.springboot.com.model.User;
+
+public interface IUserService {
+	
+	public List<User> getAllUsers();
+	
+	public User getUserById(String userId);
+	
+
+}
---
+++ src/main/java/gk/springboot/com/service/impl/UserService.java
@@ -0,0 +1,30 @@
+package gk.springboot.com.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import gk.springboot.com.mapper.UserMapper;
+import gk.springboot.com.model.User;
+import gk.springboot.com.model.UserExample;
+import gk.springboot.com.service.IUserService;
+
+@Service
+public class UserService implements IUserService {
+	
+	@Autowired
+	private UserMapper userMapper;
+
+	@Override
+	public List<User> getAllUsers() {
+		return userMapper.selectByExample(new UserExample());
+	}
+
+	@Override
+	public User getUserById(String userId) {
+		User user = userMapper.selectByPrimaryKey(userId);
+		return user;
+	}
+
+}
