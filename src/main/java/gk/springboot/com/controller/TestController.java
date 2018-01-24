08c91463f7f8ed69ffc3e421a1adb1dc2d60168a
GaoKai <gkaigk1987@gmail.com>
2018-01-24 11:32:32 +0800

    日志测试

--- src/main/java/gk/springboot/com/controller/TestController.java
+++ src/main/java/gk/springboot/com/controller/TestController.java
@@ -1,16 +1,26 @@
 package gk.springboot.com.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import gk.springboot.com.service.IUserService;
+
 @Controller
 @RequestMapping(value = "/test")
 public class TestController {
+	
+	private static Logger logger = LoggerFactory.getLogger(TestController.class);
 
+	@Autowired
+	private IUserService userService;
 	
 	@RequestMapping(value = "/index")
 	public String testHtml(Model model) {
+		userService.getAllUsers();
 		model.addAttribute("name", "gk");
 		return "test/index";
 	}
